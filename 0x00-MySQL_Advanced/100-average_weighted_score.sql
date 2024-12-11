423969863c2a04adb4b0ee67c46dc4603e58a2e8
3zzazakl <vanzekovichgr@gmail.com>
2024-12-11 20:09:18 +0200

    checking

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -5,11 +5,17 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
 BEGIN
     DECLARE weight_average_score FLOAT;
-    SET weight_average_score = (SELECT SUM(weight * score) / SUM(weight) 
+    SET weight_average_score = (
+        SELECT
+            CASE 
+                WHEN SUM(P.weight) > 0 THEN SUM(P.weight * C.score) / SUM(P.weight)
+                ELSE 0
+            END
         FROM users AS U 
         JOIN corrections AS C ON U.id=C.user_id 
         JOIN projects AS P ON C.project_id=P.id 
-        WHERE U.id=user_id);
+        WHERE U.id=user_id
+    );
 
     UPDATE users SET average_weighted_score = weight_average_score WHERE id=user_id;
 
