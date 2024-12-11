dcb0ee81391ce7214dc588d547f10f19a10b886d
3zzazakl <vanzekovichgr@gmail.com>
2024-12-11 19:24:17 +0200

    checking names

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,14 @@
+-- Safe division
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS INT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END
+$$
+DELIMITER ;
