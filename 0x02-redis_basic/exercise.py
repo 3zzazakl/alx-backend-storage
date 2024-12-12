aa86bd6d8cd9d2a15b8fa80451d48e4b1d3b00cd
3zzazakl <vanzekovichgr@gmail.com>
2024-12-12 12:02:18 +0200

    checking task0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,7 +14,6 @@ class Cache:
     """
     def __init__(self):
         self._redis = redis.Redis()
-        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
@@ -23,3 +22,29 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: callable = None) -> Union[
+        str, bytes, int, float, None]:
+        """
+        Get data from cache
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+
+        if fn:
+            return fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Get data from cache as string
+        """
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """
+        Get data from cache as integer
+        """
+        return self.get(key, fn=int)
