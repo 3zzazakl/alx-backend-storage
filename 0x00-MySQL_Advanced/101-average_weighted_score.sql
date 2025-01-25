7b2b2acebd80e969b9b29b6f74eb74f6ffbb05d8
3zzazakl <vanzekovichgr@gmail.com>
2025-01-25 14:26:18 +0200

    setting files

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,37 +1,20 @@
 -- Create procedure
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER $$
-
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DECLARE user_id INT;
-    DECLARE total_weight INT;
-    DECLARE weighted_sum FLOAT;
-    DECLARE done INT DEFAULT 0;
-    DECLARE cur CURSOR FOR SELECT id FROM users;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+    UPDATE users as U,
+    (SELECT U.id, SUM(score * weight) / SUM(weight) AS weighted_score
+    from users as U
+    join corrections as C on U.id=C.user_id
+    join projects as P on C.project_id=P.id
+    group by U.id) as WA
+    SET U.average_score = WA.weighted_score
+    WHERE U.id=WA.id;
 
-    OPEN cur;
-
-    read_loop: LOOP
-        FETCH cur INTO user_id;
-        IF done THEN
-            LEAVE read_loop;
-        END IF;
-
-        SELECT SUM(c.score * p.weight), SUM(p.weight) INTO weighted_sum, total_weight
-            FROM corrections AS c
-            JOIN projects AS p ON c.project_id=p.id
-            WHERE c.user_id=user_id;
-
-            if total_weight = 0 then
-                UPDATE users SET average_score = weighted_sum / total_weight
-                WHERE id=user_id;
-            ELSE
-                UPDATE users SET average_score = 0 WHERE id=user_id;
-            END IF;
-        END LOOP;
-        CLOSE cur;
-
-END $$
-
+END
+$$
 DELIMITER ;
+
+
+
