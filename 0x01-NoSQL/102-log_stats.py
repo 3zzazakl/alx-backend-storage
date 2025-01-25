471835d48a33c2646c34796de29411e0c021d59b
3zzazakl <vanzekovichgr@gmail.com>
2025-01-25 16:47:56 +0200

    fixing error

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -14,15 +14,15 @@ def log_stats():
     logs_collection = client.logs.nginx
     count = logs_collection.count_documents({})
     print("{} logs".format(count))
-    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        meth_count = logs_collection.count_documents({"method": {
-            "$eq": method}})
-        print("\tmethod {}: {}".format(method, meth_count))
+        meth_count = logs_collection.count_documents({"method": method})
+        print("\tmethod: {}: {}".format(method, meth_count))
 
     status_checks = logs_collection.count_documents({
-        "$and": [{"method": {"$eq": "GET"}}, {"path": "/status"}]})
+        "method": "GET",
+        "path": "/status"
+    })
     print("{} status check".format(status_checks))
 
 
