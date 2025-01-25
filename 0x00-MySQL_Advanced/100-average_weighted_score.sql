e383058e94ff08c47fb1b1c6f9b6d4513df0b35d
3zzazakl <vanzekovichgr@gmail.com>
2025-01-25 14:17:25 +0200

    setting files

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -6,20 +6,15 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
 BEGIN
     DECLARE weight_average_score FLOAT;
     SET weight_average_score = (
-        SELECT
-            CASE 
-                WHEN SUM(P.weight) > 0 THEN SUM(P.weight * C.score) / SUM(P.weight)
-                ELSE 0
-            END
-        FROM users AS U 
-        JOIN corrections AS C ON U.id=C.user_id 
-        JOIN projects AS P ON C.project_id=P.id 
-        WHERE U.id=user_id
+        SELECT SUM(c.score * p.weight) / SUM(p.weight)
+        FROM corrections AS c
+        JOIN projects AS p ON c.project_id=p.id
+        JOIN users AS u ON c.user_id=u.id
+        WHERE u.id=user_id
     );
+    
+    UPDATE users SET average_score = weight_average_score WHERE id=user_id;
 
-    UPDATE users SET average_weighted_score = weight_average_score WHERE id=user_id;
-
-END 
+END
 $$
-
 DELIMITER ;
