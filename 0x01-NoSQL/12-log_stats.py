c057a23160c0581525611c515e679c647e3a5bff
3zzazakl <vanzekovichgr@gmail.com>
2025-01-25 16:55:20 +0200

    setting files

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -11,28 +11,19 @@ from pymongo import MongoClient
 def log_stats():
     """ log stats """
     client = MongoClient('mongodb://127.0.0.1:27017')
-
-    db = client.my_db
-    school_collection = db.school
-
-    total_logs = school_collection.count_documents({})
-
-    methods = {"GET", "POST", "PUT", "DELETE", "PATCH"}
-    methods_counts = {}
+    logs_collection = client.logs.nginx
+    count = logs_collection.count_documents({})
+    print("{} logs".format(count))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        methods_counts[method] = school_collection.count_documents({
-            "method": method})
+        meth_count = logs_collection.count_documents({"method": method})
+        print("\tmethod: {}: {}".format(method, meth_count))
 
-    status_check = school_collection.count_documents({
+    status_checks = logs_collection.count_documents({
         "method": "GET",
-        "path": "/status"})
-
-    print(f"{total_logs} logs")
-    print("Methods:")
-    for method in methods:
-        print(f"\tmethod {method}: {methods_counts[method]}")
-
-    print(f"{status_check} status checks")
+        "path": "/status"
+    })
+    print("{} status check".format(status_checks))
 
 
 if __name__ == "__main__":
